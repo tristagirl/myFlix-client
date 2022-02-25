026e1a1116e1c578de355c843286c75b8ea926f9
Trista <tdahleen@me.com>
2022-02-25 16:05:36 -0400

    updates 3.4
    new branch

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,42 +1,85 @@
 import React from 'react';
+//importing axios library to fetch movies from database
+import axios from 'axios';
+
+
+//importing the registration view into the main-view
+import { RegistrationView } from '../registration-view/registration-view';
+//importing the login view into the main-view
+import { LoginView } from '../login-view/login-view';
+//importing the movie-card into the main-view
 import { MovieCard } from '../movie-card/movie-card';
+////importing the movie-view into the main-view
 import { MovieView } from '../movie-view/movie-view';
 
-let imgPath = './img';
-
-export default class MainView extends React.Component {
+export class MainView extends React.Component {
 
   constructor(){
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: imgPath + 'inception.png', Genre:'Action, Adventure, Sci-fi', Director:'Christopher Nolan'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: imgPath + 'shawshank.jpg', Genre:'Drama', Director:'Frank Darabont'},
-        { _id: 3, Title: "Harry Potter and the Sorcerer's Stone", Description: 'An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.', ImagePath: imgPath + 'harrypotter.jpg', Genre:'Adventure, Family, Fantasy', Director:'Chris Columbus'}
-      ],
-      selectedMovie: null
-    }
+      movies: [],
+      selectedMovie: null,
+      user: null
+    };
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  componentDidMount(){
+    axios.get('https://immense-reef-38292.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  /*When a movie is clicked, this function is invoked and updates the state of
+  the `selectedMovie` *property to that movie*/
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
+    });
+  }
+
+  //When a user successfully registers
+  onRegistration(register) {
+    this.setState({
+      register,
+    });
+  }
+
+  /* When a user successfully logs in, this function updates the `user`
+  property in state to that *particular user*/
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
 
   render() {
-    const { movies, selectedMovie } = this.state;
-  
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-  
+    const { movies, selectedMovie, user, register } = this.state;
+
+    if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)}/>);
+
+    /* If there is no user, the LoginView is rendered. If there is a user
+    logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return (<LoginView onLoggedIn={user => this.onLoggedIn(user)} />);
+
+    // Before the movies have been loaded
+    if (movies.length === 0) return (<div className="main-view" />);
+
     return (
-        <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-          : movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />)
-        }
-      </div>
-    );
+           <div className="main-view">
+               {selectedMovie
+                   ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+                   : movies.map(movie => (<MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} /> ))
+               }
+           </div>
+       );
+
   }
+
 }
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 export class MovieView extends React.Component {
+
+  keypressCallback(event) {
+    console.log(event.key);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keypress', this.keypressCallback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
 
   render() {
     const { movie, onBackClick } = this.props;
@@ -14,20 +27,35 @@ export class MovieView extends React.Component {
           <span className="label">Title: </span>
           <span className="value">{movie.Title}</span>
         </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre}</span>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director}</span>
-        </div>
         <div className="movie-description">
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
+
        </div>
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+};
